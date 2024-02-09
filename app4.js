4704ff3f8a7b77cac77ee54e22d78b18fcdf45b4
jhonmeireles <jhonathan.oliveira@delage.com.br>
2024-02-08 21:28:27 -0300

    atuali

--- app4.js
+++ app4.js
@@ -206,9 +206,9 @@ client.on("ready", () => {
     }
 });
 
-client.on("qr", (qr) => {
-    qrcode.generate(qr, { small: true });
-});
+// client.on("qr", (qr) => {
+//     qrcode.generate(qr, { small: true });
+// });
 
 // Opcional: código para listar grupos
 client.on("ready", () => {
